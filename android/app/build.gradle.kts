ddb86577cbbc30c1633160937643c9ad63643f90
skycreator1 <96583554+skycreator1@users.noreply.github.com>
2026-05-08 21:14:57 +0800

    ci(android): downgrade kotlin plugin for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,7 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
